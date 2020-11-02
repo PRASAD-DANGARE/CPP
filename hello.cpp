aca8c572361d657eec2c688ff4ad0592a051bea1
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-11-02 16:22:22 +0530

    Simple Program Which Display A Message

---
+++ hello.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // Header File(often Subdirectory Called include) And Insert It.
+using namespace std; // Declarations,Definations In namespace eg cout,cin namespace std Is Used With Header File
+
+int main() // Entry Point Function
+{
+    cout << "Hello Im Prasad " // cout Use For Display It Into Console And cin is Use For Input From Keyboard
+         << "Dangare!..." << endl;
+}
