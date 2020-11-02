d8f81a1415ab9475f6e0820db6a3079ae38f0d6a
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-11-02 17:12:31 +0530

    Call Another Program

---
+++ callhello.cpp
@@ -0,0 +1,17 @@
+/*
+
+Description      :  Call Another Program
+Function Date    :  2 Nov 2020
+Function Author  :  Prasad Dangare
+Input            :  --
+
+*/
+
+#include <cstdlib> // Declare "System()"
+
+using namespace std;
+
+int main()
+{
+    system("hello");
+}
