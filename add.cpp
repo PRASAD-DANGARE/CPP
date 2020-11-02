7f4d5b24f3a746e2f851a782997ba4aacc03df4b
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-11-02 19:38:40 +0530

    Addition Of Two Numbers

---
+++ add.cpp
@@ -0,0 +1,26 @@
+/*
+
+Description      :  Addition Of Two Numbers
+Function Date    :  2 Nov 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int No1, No2, Ans;
+
+    cout << "Enter Two Numbers : ";
+    cin >> No1 >> No2;
+
+    Ans = No1 + No2;
+
+    cout << No1 << " + " << No2 << " = " << Ans;
+
+    return 0;
+}
