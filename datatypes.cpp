6df8ea1696ae7de3212e60320db6acc2ff64a8fe
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-11-06 17:16:06 +0530

    Basic Four Datatypes In C++

---
+++ datatypes.cpp
@@ -0,0 +1,33 @@
+/*
+
+Description      :  Basic Four Datatypes In C++
+Function Date    :  6 Nov 2020
+Function Author  :  Prasad Dangare
+Input            :  Char,Integer,Float,Double
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Defination Without Initialization:
+
+    char Prasad;
+    int Number;
+    float Area;
+    double Value;
+
+    // Simultaneous Defination And Initialization:
+
+    char first = 'A', second = 'Z';
+    int sum = 100, sum2 = 150,
+        sum3 = 200;
+    float radious = 3.1419;
+
+    // Exponential Notation:
+
+    double volume = 6e-4;
+}
