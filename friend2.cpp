1f392d2353c1df195f7eb5a28501ea0806ca2cda
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-11-25 17:38:06 +0530

    A Function Friendly To Two Class

---
+++ friend2.cpp
@@ -0,0 +1,67 @@
+/*
+Description      :  A Function Friendly To Two Class
+Function Date    :  25 Nov 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+*/
+
+#include <iostream>
+
+using namespace std;
+
+class ABC; // fordward declaration
+
+//-----------------------------------------//
+
+class XYZ
+{
+    int x;
+
+    public:
+      void setvalue(int i)
+      {
+          x = i;
+      }
+
+      friend void max(XYZ, ABC);
+};
+
+//-------------------------------------------//
+
+class ABC
+{
+    int a;
+
+    public:
+      void setvalue(int i)
+      {
+          a = i;
+      }
+
+      friend void max(XYZ, ABC);
+};
+
+//-------------------------------------------//
+
+void max(XYZ m, ABC n) // defination of friend
+{
+    if(m.x >= n.a)
+      cout << m.x;
+    else
+      cout << n.a;
+}
+
+//-------------------------------------------//
+
+int main()
+{
+    ABC abc;
+    abc.setvalue(10);
+    
+    XYZ xyz;
+    xyz.setvalue(20);
+
+    max(xyz, abc);
+    return 0;
+}
+
