a44cd8263d83fc57bf48fa65efe95f153e8e1478
Prasad_Dangare <67819815+PRASAD-DANGARE@users.noreply.github.com>
2020-12-08 16:18:52 +0530

    Working With Multiple Files

---
+++ file2.cpp
@@ -0,0 +1,62 @@
+/*
+Description      :  Working With Multiple Files
+Function Date    :  8 Dec 2020
+Function Author  :  Prasad Dangare
+Input            :  Integer
+*/
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    ofstream fout;
+    fout.open("country");
+
+    fout << "United States Of America, \n";
+    fout << "United Kingdom, \n";
+    fout << "United Korea \n";
+
+    fout.close();
+
+    fout.open("capital");
+
+    fout  << "Washington, \n";
+    fout  << "London, \n";
+    fout  << "Seoul \n";
+
+    fout.close();
+
+    const int N = 80;
+    char line[N];
+
+    ifstream fin;
+    
+    fin.open("country");
+
+    cout << "\n --- Constents Of Country File ---\n\n";
+
+    while (fin)
+    {
+        fin.getline(line, N);
+        cout << line;
+    }
+
+    fin.close();
+    
+    fin.open("capital");
+    
+    cout << "\n\n --- Contents Of Capital File ---\n\n";
+
+    while (fin)
+    {
+        fin.getline(line, N);
+        cout << line;
+    }
+
+    fin.close();
+
+    return 0;
+}
